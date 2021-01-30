e7105f192c9f0fd7bb9040ae6c1c132bfe0ae71e
Graham Bennett <graham@grahambennett.org>
2021-01-30 23:49:44 +0000

    Add power sensor
    Note, this returns zero power when node is not active.  The heater seems to report power usage all the time otherwise, which doesn't make sense and doesn't tally with the graphs in the vendor app UI.

--- custom_components/smartbox/sensor.py
+++ custom_components/smartbox/sensor.py
@@ -2,6 +2,8 @@ import logging
 
 from homeassistant.const import (
     DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_POWER,
+    POWER_WATT,
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
 )
@@ -21,55 +23,76 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     async_add_entities(
         [TemperatureSensor(node) for node in hass.data[DOMAIN][SMARTBOX_NODES] if node.node_type == 'htr'], True)
+    async_add_entities([PowerSensor(node) for node in hass.data[DOMAIN][SMARTBOX_NODES] if node.node_type == 'htr'],
+                       True)
 
     _LOGGER.debug("Finished setting up Smartbox sensor platform")
 
 
-class TemperatureSensor(Entity):
-    """Smartbox heater temperature sensor"""
-
-    device_class = DEVICE_CLASS_TEMPERATURE
-
+class SmartboxSensorBase(Entity):
     def __init__(self, node):
-        """Initialize the sensor."""
         self._node = node
         _LOGGER.debug(f"Created node {self.name} unique_id={self.unique_id}")
 
     @property
-    def unique_id(self):
-        """Return Unique ID string."""
-        return f"{self._node.node_id}_temperature"
+    def name(self):
+        return self._node.name
 
     @property
     def device_state_attributes(self):
-        """Return the state attributes of the device."""
         return {
             ATTR_LOCKED: self._node.status['locked'],
         }
 
     @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._node.status['mtemp']
-
-    @property
     def available(self) -> bool:
-        """Return True if roller and hub is available."""
         return self._node.status['sync_status'] == 'ok'
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS if self._node.status['units'] == 'C' else TEMP_FAHRENHEIT
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._node.name
 
     async def async_update(self):
         _LOGGER.debug("Smartbox sensor async_update")
         await self._node.async_update(self.hass)
 
 
-# TODO: power sensor?
+class TemperatureSensor(SmartboxSensorBase):
+    """Smartbox heater temperature sensor"""
+
+    device_class = DEVICE_CLASS_TEMPERATURE
+
+    def __init__(self, node):
+        super().__init__(node)
+
+    @property
+    def unique_id(self):
+        return f"{self._node.node_id}_temperature"
+
+    @property
+    def state(self):
+        return self._node.status['mtemp']
+
+    @property
+    def unit_of_measurement(self):
+        return TEMP_CELSIUS if self._node.status['units'] == 'C' else TEMP_FAHRENHEIT
+
+
+class PowerSensor(SmartboxSensorBase):
+    """Smartbox heater power sensor"""
+
+    device_class = DEVICE_CLASS_POWER
+
+    def __init__(self, node):
+        super().__init__(node)
+
+    @property
+    def unique_id(self):
+        return f"{self._node.node_id}_power"
+
+    @property
+    def state(self):
+        # TODO: is this correct? The heater seems to report power usage all the
+        # time otherwise, which doesn't make sense and doesn't tally with the
+        # graphs in the vendor app UI
+        return self._node.status['power'] if self._node.status['active'] else 0
+
+    @property
+    def unit_of_measurement(self):
+        return POWER_WATT
