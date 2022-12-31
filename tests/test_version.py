084bcf8d6809a305293df8f71eff98f5a4597044
Graham Bennett <graham@grahambennett.org>
2022-12-31 12:42:29 +0000

    Fix version

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,4 +3,6 @@ import custom_components.smartbox
 
 
 def test_version():
-    assert re.match(r"^[0-9\.]+$", custom_components.smartbox.__version__)
+    assert re.match(
+        r"^[0-9\.]+(-[a-z0-9\.]+)?$", custom_components.smartbox.__version__
+    )
