c7170e4ff9f6639d293c93663a345a18153956e0
Graham Bennett <graham@grahambennett.org>
2023-01-02 22:52:55 +0000

    Fix version script

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -18,13 +18,13 @@ requirements_smartbox_version() {
     sed -n -e 's/^.*\(smartbox==[0-9\.]\+\(b[0-9]\+\)\?\).*$/\1/p' requirements_common.txt
 }
 
-if [ $(manifest_version) != $(module_version) ]
+if [ "$(manifest_version)" != "$(module_version)" ]
 then
     echo "Manifest version $(manifest_version) does not match module $(module_version)" >&2
     exit 1
 fi
 
-if [ $(manifest_smartbox_version) != $(requirements_smartbox_version) ]
+if [ "$(manifest_smartbox_version)" != "$(requirements_smartbox_version)" ]
 then
     echo "Manifest smartbox version $(manifest_smartbox_version) does not match requirements smartbox version $(requirements_smartbox_version)" >&2
     exit 1
