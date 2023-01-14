b5428b35ba75c29cbce6a69bfa05488e3adaab24
Graham Bennett <graham@grahambennett.org>
2023-01-14 21:38:42 +0000

    Re-enable mypy in pre-commit-checks.sh

--- scripts/pre-commit-check.sh
+++ scripts/pre-commit-check.sh
@@ -18,18 +18,25 @@ requirements_smartbox_version() {
     sed -n -e 's/^.*\(smartbox==[0-9\.]\+\(b[0-9]\+\)\?\).*$/\1/p' requirements_common.txt
 }
 
+echo Checking manifest and module versions match...
 if [ "$(manifest_version)" != "$(module_version)" ]
 then
     echo "Manifest version $(manifest_version) does not match module $(module_version)" >&2
     exit 1
 fi
 
+echo Checking manifest and requirements smartbox versions match...
 if [ "$(manifest_smartbox_version)" != "$(requirements_smartbox_version)" ]
 then
     echo "Manifest smartbox version $(manifest_smartbox_version) does not match requirements smartbox version $(requirements_smartbox_version)" >&2
     exit 1
 fi
 
+echo Checking black formatting...
 black --check .
 
+echo Checking flake8...
 flake8
+
+echo Checking mypy...
+mypy .
